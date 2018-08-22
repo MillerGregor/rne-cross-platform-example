9485b1e2ffd2669ca243cb6747d282bca008ba41
Greg Miller <millergregor@gmail.com>
2018-08-22 10:19:51 -0400

    test husky

---
+++ scripts/pre-push-hook.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const ALLOW_PUSH = ".allowPush";
+
+console.log(env.GIT_PARAMS);
+if (!fs.existsSync(path.resolve(__dirname, ALLOW_PUSH))) {
+  throw new Error(`*** ${ALLOW_PUSH} file not found. push aborted ***`);
+}
